bf0387d2cc00fcf38319ccc2f8e60a2cfce23f02
GALAXYBOOK\pc <0ptimist0@naver.com>
2020-08-09 04:33:09 +0900

    재명:: 수정

--- admin/application/modules/home/views/view_home.php
+++ admin/application/modules/home/views/view_home.php
@@ -1,6 +1,7 @@
 <div class="test_wrap">
 	<p id="test">밍고 초기 진입 화면!!</p>
 	<div id="like_button_container"></div>
+	<img src="/images/user_default.png" alt="">
 </div>
 
 <script type="text/babel" src="/reactjs/like_button.js" ></script>
