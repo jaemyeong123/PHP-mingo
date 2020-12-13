b5e0bf5b54f64c8d4cc4859f29c43724ed0cd50b
GALAXYBOOK\pc <0ptimist0@naver.com>
2020-12-13 22:47:58 +0900

    mingo:: 수정

--- react/src/index.js
+++ react/src/index.js
@@ -1,32 +1,33 @@
 //------------- 리덕스 사용시 ----------------
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import store from './app/store';
-// import { Provider } from 'react-redux';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// serviceWorker.unregister();
+serviceWorker.unregister();
 
 //------------- 리덕스 사용시 ----------------
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './client/Root';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import Root from './client/Root';
+// import Root from './src/App';
 // import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+// import './index.css';
+//
+// ReactDOM.render(<Root />, document.getElementById('root'));
 // registerServiceWorker();
