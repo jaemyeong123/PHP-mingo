575a0ac78e69d10b2cff6604052c0634794ec039
GALAXYBOOK\pc <0ptimist0@naver.com>
2021-02-10 21:59:10 +0900

    jm::[react] abour router

--- react/src/shared/App.js
+++ react/src/shared/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home, About } from '../pages';
+import { Home, About, Menu } from '../pages';
+
+
 
 
 class App extends Component {
     render() {
-        return (
+        return (          
             <div>
-                <Route exact path="/" component={Home}/>
-                <Switch>
-                    <Route path="/about/:name" component={About}/>
-                    <Route path="/about" component={About}/>
-                </Switch>
+              <Menu></Menu>
+              <div>
+                  <Route exact path="/" component={Home}/>
+                  <Switch>
+                      <Route path="/about/:name" component={About}/>
+                      <Route path="/about" component={About}/>
+                  </Switch>
 
+              </div>
             </div>
+
         );
     }
 }
