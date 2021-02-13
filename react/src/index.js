1661030d2c76b554d96cefdb7aa67ea67b37aae0
GALAXYBOOK\pc <0ptimist0@naver.com>
2021-02-14 01:26:06 +0900

    jm::[react] qr코드 추가

--- react/src/index.js
+++ react/src/index.js
@@ -1,33 +1,6 @@
-//------------- 리덕스 사용시 ----------------
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import store from './app/store';
-// import { Provider } from 'react-redux';
-// import * as serviceWorker from './serviceWorker';
-//
-//
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-//
-// serviceWorker.unregister();
-
-//------------- 리덕스 사용시 ----------------
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './client/Root';
 import './index.css';
-// import Root from './src/App';
-// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-// registerServiceWorker();
